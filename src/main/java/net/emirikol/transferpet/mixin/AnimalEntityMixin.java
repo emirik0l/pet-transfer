95e4d13889856b5afc8261259bc868c43f1a6edd
ofasgard <77687278+emirik0l@users.noreply.github.com>
2022-01-12 20:24:01 +0000

    add a mixin to make contract fulfilment work on wolves and other right-clickable pets

---
+++ src/main/java/net/emirikol/transferpet/mixin/AnimalEntityMixin.java
@@ -0,0 +1,34 @@
+package net.emirikol.transferpet.mixin;
+
+import net.emirikol.transferpet.PetTransfer;
+import net.emirikol.transferpet.item.PetContract;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import javax.swing.*;
+
+@Mixin(AnimalEntity.class)
+public abstract class AnimalEntityMixin extends PassiveEntity {
+    public AnimalEntityMixin(EntityType<? extends PassiveEntity> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(method = "interactMob", at = @At("TAIL"), cancellable = true)
+    protected void interactWithContract(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cb) {
+        ItemStack stack = player.getStackInHand(hand);
+        if (stack.getItem() instanceof PetContract) {
+            ActionResult result = PetTransfer.PET_CONTRACT.useOnEntity(stack, player, this, hand);
+            cb.setReturnValue(result);
+        }
+    }
+}
