29571c0c2fe2c439a4cd1af65173ed7d0c22e581
ofasgard <77687278+emirik0l@users.noreply.github.com>
2022-01-14 17:43:17 +0000

    add support for HorseBaseEntity, thanks to deirn

--- src/main/java/net/emirikol/transferpet/item/PetContract.java
+++ src/main/java/net/emirikol/transferpet/item/PetContract.java
@@ -2,6 +2,7 @@ package net.emirikol.transferpet.item;
 
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.HorseBaseEntity;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
@@ -37,9 +38,9 @@ public class PetContract extends Item {
     @Override
     public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
         if (this.isContractFilled(stack)) {
-            Text petText = new TranslatableText("text.transferpet.tooltip_pet").formatted(Formatting.DARK_GRAY).append(this.getPetName(stack));
+            Text petText = new TranslatableText("text.transferpet.tooltip_pet").formatted(Formatting.DARK_GRAY).append(this.getContractPetName(stack));
             tooltip.add(petText);
-            Text ownerText = new TranslatableText("text.transferpet.tooltip_owner").formatted(Formatting.DARK_GRAY).append(this.getOwnerName(stack));
+            Text ownerText = new TranslatableText("text.transferpet.tooltip_owner").formatted(Formatting.DARK_GRAY).append(this.getContractOwnerName(stack));
             tooltip.add(ownerText);
         }
     }
@@ -56,7 +57,7 @@ public class PetContract extends Item {
             //Check if the target matches the contract.
             if (isContractValid(stack, entity)) {
                 //If it does, perform the transfer of ownership.
-                this.transferOwnership(user, (TameableEntity) entity);
+                this.transferOwnership(user, entity);
                 stack.decrement(1);
                 entity.world.playSound(null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.ITEM_BOOK_PAGE_TURN, SoundCategory.NEUTRAL, 1.5F, 1.0F + (entity.world.random.nextFloat() - entity.world.random.nextFloat()) * 0.4F);
             } else {
@@ -67,7 +68,7 @@ public class PetContract extends Item {
             //If contract is not filled, check if target is owned by the player.
             if (this.isTargetOwned(user, entity)) {
                 //If the target is owned by the player, fill the contract.
-                this.fillContract(stack, user, (TameableEntity) entity);
+                this.fillContract(stack, user, entity);
                 entity.world.playSound(null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.ITEM_BOOK_PAGE_TURN, SoundCategory.NEUTRAL, 1.5F, 1.0F + (entity.world.random.nextFloat() - entity.world.random.nextFloat()) * 0.4F);
             } else {
                 //If the target is not owned by the player, inform them of their mistake.
@@ -78,7 +79,7 @@ public class PetContract extends Item {
         return ActionResult.SUCCESS;
     }
 
-    public void fillContract(ItemStack stack, PlayerEntity player, TameableEntity entity) {
+    public void fillContract(ItemStack stack, PlayerEntity player, LivingEntity entity) {
         ItemStack filled = new ItemStack(this, 1);
         stack.decrement(1);
         NbtCompound nbt = filled.getOrCreateNbt();
@@ -91,17 +92,17 @@ public class PetContract extends Item {
         inventory.offerOrDrop(filled);
     }
 
-    public void transferOwnership(PlayerEntity player, TameableEntity entity) {
+    public void transferOwnership(PlayerEntity player, LivingEntity entity) {
         String playerName = player.getDisplayName().getString();
         String entityName = entity.getDisplayName().getString();
-        LivingEntity oldOwner = entity.getOwner();
+        LivingEntity oldOwner = this.getEntityOwner(entity);
         if (oldOwner instanceof PlayerEntity && !oldOwner.world.isClient) {
             ((PlayerEntity) oldOwner).sendMessage(new TranslatableText("text.transferpet.message_old_owner", entityName, playerName), false);
         }
         if (!player.world.isClient) {
             player.sendMessage(new TranslatableText("text.transferpet.message_new_owner", entityName), false);
         }
-        entity.setOwner(player);
+        this.setEntityOwner(entity, player);
     }
 
     public boolean isContractFilled(ItemStack stack) {
@@ -109,37 +110,61 @@ public class PetContract extends Item {
     }
 
     public boolean isTargetOwned(PlayerEntity player, LivingEntity entity) {
-        if (!(entity instanceof TameableEntity)) return false;
-        TameableEntity tameableEntity = (TameableEntity) entity;
-        return tameableEntity.getOwnerUuid() == player.getUuid();
+        if (!this.isValidEntity(entity)) return false;
+        return this.getEntityOwnerUUID(entity) == player.getUuid();
     }
 
     public boolean isContractValid(ItemStack stack, LivingEntity entity) {
         //Contracts are valid if the UUID of the entity matches the UUID of the contract.
         //The owner UUID in the contract must also match the current owner UUID of the entity (outdated contracts are invalid).
-        if (!(entity instanceof TameableEntity)) return false;
-        TameableEntity tameableEntity = (TameableEntity) entity;
-        return (tameableEntity.getUuid().equals(this.getPet(stack))) && (tameableEntity.getOwnerUuid().equals(this.getOwner(stack)));
+        if (!this.isValidEntity(entity)) return false;
+        return (entity.getUuid().equals(this.getContractPet(stack))) && (this.getEntityOwnerUUID(entity).equals(this.getContractOwner(stack)));
     }
 
-    public UUID getPet(ItemStack stack) {
+    public UUID getContractPet(ItemStack stack) {
         NbtCompound nbt = stack.getOrCreateNbt();
         return UUID.fromString(nbt.getString("contract_uuid"));
     }
 
-    public String getPetName(ItemStack stack) {
+    public String getContractPetName(ItemStack stack) {
         NbtCompound nbt = stack.getOrCreateNbt();
         return nbt.getString("contract_name");
     }
 
-    public UUID getOwner(ItemStack stack) {
+    public UUID getContractOwner(ItemStack stack) {
         NbtCompound nbt = stack.getOrCreateNbt();
         return UUID.fromString(nbt.getString("contract_owner_uuid"));
     }
 
-    public String getOwnerName(ItemStack stack) {
+    public String getContractOwnerName(ItemStack stack) {
         NbtCompound nbt = stack.getOrCreateNbt();
         return nbt.getString("contract_owner_name");
+    }
+
+    //Helper methods because for some reason horses don't extend TameableEntity.
+
+    public boolean isValidEntity(LivingEntity entity) {
+        return entity instanceof TameableEntity || entity instanceof HorseBaseEntity;
+    }
+
+    public LivingEntity getEntityOwner(LivingEntity entity) {
+        if (entity instanceof TameableEntity) { return ((TameableEntity) entity).getOwner(); }
+        if (entity instanceof HorseBaseEntity) {
+            UUID ownerUUID = ((HorseBaseEntity) entity).getOwnerUuid();
+            return ownerUUID == null ? null : entity.world.getPlayerByUuid(ownerUUID);
+        }
+        return null;
+    }
+
+    public UUID getEntityOwnerUUID(LivingEntity entity) {
+        if (entity instanceof TameableEntity) { return ((TameableEntity) entity).getOwnerUuid(); }
+        if (entity instanceof HorseBaseEntity) { return ((HorseBaseEntity) entity).getOwnerUuid(); }
+        return null;
+    }
+
+    public void setEntityOwner(LivingEntity entity, PlayerEntity player) {
+        if (entity instanceof TameableEntity) { ((TameableEntity) entity).setOwner(player); }
+        if (entity instanceof HorseBaseEntity) { ((HorseBaseEntity) entity).bondWithPlayer(player); }
     }
 }
 
