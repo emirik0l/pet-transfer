33a49cbb5e981433c396c0c297e7f49db475c354
ofasgard <77687278+emirik0l@users.noreply.github.com>
2022-01-16 14:13:16 +0000

    unused import

--- src/main/java/net/emirikol/transferpet/item/CreativeContract.java
+++ src/main/java/net/emirikol/transferpet/item/CreativeContract.java
@@ -1,8 +1,6 @@
 package net.emirikol.transferpet.item;
 
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 
 public class CreativeContract extends PetContract {
     public CreativeContract(Settings settings) {
