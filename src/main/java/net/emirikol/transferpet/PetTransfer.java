205ac8107ff124407fa0867415be7ec5931d1f48
emirik0l <77687278+emirik0l@users.noreply.github.com>
2025-01-05 15:31:29 +0000

    begin updating to 1.21.4
    does not build

--- src/main/java/net/emirikol/transferpet/PetTransfer.java
+++ src/main/java/net/emirikol/transferpet/PetTransfer.java
@@ -4,23 +4,43 @@ import net.emirikol.transferpet.item.CreativeContract;
 import net.emirikol.transferpet.item.PetContract;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
 
 public class PetTransfer implements ModInitializer {
+	public static final String MOD_ID = "transferpet";
+	
 	public static PetContract PET_CONTRACT;
 	public static CreativeContract CREATIVE_CONTRACT;
 
 	@Override
 	public void onInitialize() {
 		//Instantiate contract.
-		FabricItemSettings petContractSettings = new FabricItemSettings();
-		petContractSettings.group(ItemGroup.MISC);
-		PET_CONTRACT = new PetContract(petContractSettings);
-		CREATIVE_CONTRACT = new CreativeContract(petContractSettings);
+		Identifier contract_id = Identifier.of(MOD_ID, "contract");
+		RegistryKey<Item> contract_key = RegistryKey.of(RegistryKeys.ITEM, contract_id);
+		Item.Settings contract_settings = new Item.Settings().useItemPrefixedTranslationKey().registryKey(contract_key);
+		
+		Identifier contract_creative_id = Identifier.of(MOD_ID, "contract_creative");
+		RegistryKey<Item> contract_creative_key = RegistryKey.of(RegistryKeys.ITEM, contract_creative_id);
+		Item.Settings contract_creative_settings = new Item.Settings().useItemPrefixedTranslationKey().registryKey(contract_creative_key);
+		
+		PET_CONTRACT = new PetContract(contract_settings);
+		CREATIVE_CONTRACT = new CreativeContract(contract_creative_settings);
+		
+		// Add items to item groups.
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register((itemGroup) -> itemGroup.add(PET_CONTRACT));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register((itemGroup) -> itemGroup.add(CREATIVE_CONTRACT));
+		
 		//Register contract.
-		Registry.register(Registry.ITEM, "transferpet:contract", PET_CONTRACT);
-		Registry.register(Registry.ITEM, "transferpet:contract_creative", CREATIVE_CONTRACT);
+		Registry.register(Registries.ITEM, contract_id, PET_CONTRACT);
+		Registry.register(Registries.ITEM, contract_creative_id, CREATIVE_CONTRACT);
 	}
 }
