4ba65356b63495b20254a6df2c8961afb1908e8c
emirik0l <77687278+emirik0l@users.noreply.github.com>
2025-01-05 15:31:26 +0000

    add basic logic for contract creation
    the "transfer ownership" part is still not implemented, and it doesn't actually work on most tameables because they just sit and don't actually call useOnEntity()

--- src/main/java/net/emirikol/transferpet/item/PetContract.java
+++ src/main/java/net/emirikol/transferpet/item/PetContract.java
@@ -1,16 +1,41 @@
 package net.emirikol.transferpet.item;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 
 import java.util.UUID;
 
 public class PetContract extends Item {
     public PetContract(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+        //Check if contract is filled.
+        if (this.isContractFilled(stack)) {
+            return super.useOnEntity(stack, user, entity, hand);
+            //TODO
+        } else {
+            //If contract is not filled, check if target is owned by the player.
+            if (entity instanceof TameableEntity && ((TameableEntity) entity).getOwner() == user) {
+                //If the target is owned by the player, fill the contract.
+                this.fillContract(stack, user, (TameableEntity) entity);
+                return ActionResult.SUCCESS;
+            } else {
+                //If the target is not owned by the player, inform them of their mistake.
+                if (!user.getWorld().isClient) { user.sendMessage(new TranslatableText("text.transferpet.contract_fail"), true); }
+                return ActionResult.PASS;
+            }
+        }
+
     }
 
     public void fillContract(ItemStack stack, PlayerEntity player, TameableEntity entity) {
